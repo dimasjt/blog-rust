b8c2cd43e576e2f4ec79139781b5d8ef03075995
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-08-24 14:49:38 +0700

    suicide

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,29 @@ extern crate iron;
 extern crate logger;
 extern crate env_logger;
 extern crate router;
+extern crate bodyparser;
+extern crate persistent;
+extern crate rustc_serialize;
+
+// #[macro_use] extern crate serde_derive;
 
 use iron::prelude::*;
 use iron::{AfterMiddleware};
 use iron::error::{IronError};
 use iron::status;
-
-use logger::Logger;
+// use logger::Logger;
 use router::{Router, NoRoute};
+use persistent::Read;
+use rustc_serialize::json;
 
 struct Custom404;
+
+#[derive(RustcEncodable)]
+struct Article {
+    title: String
+}
+
+const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
 
 impl AfterMiddleware for Custom404 {
     fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
@@ -26,14 +39,14 @@ impl AfterMiddleware for Custom404 {
 fn main() {
     env_logger::init().unwrap();
 
-    let (logger_before, _) = Logger::new(None);
+    // let (logger_before, _) = Logger::new(None);
     let mut router = Router::new();
 
     router.get("/", home_ctrl, "home");
     router.get("/posts/:id", show_ctrl, "show");
 
     let mut chain = Chain::new(router);
-    chain.link_before(logger_before);
+    chain.link_before(Read::<bodyparser::MaxBodyLength>::one(MAX_BODY_LENGTH));
     chain.link_after(Custom404);
 
     match Iron::new(chain).http("localhost:3000") {
@@ -43,7 +56,10 @@ fn main() {
 }
 
 fn home_ctrl(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Home Page")))
+    let article = Article { title: "Learning Rust".to_string() };
+    let arrs: Vec<Article> = vec![article];
+    let payload = json::encode(&arrs).unwrap();
+    Ok(Response::with((status::Ok, payload)))
 }
 
 fn show_ctrl(req: &mut Request) -> IronResult<Response> {
